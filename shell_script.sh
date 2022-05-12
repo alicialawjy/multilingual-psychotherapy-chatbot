37774c18c56982665e423a60c621b875ea7729e6
alicialawjy <alicialawjy@gmail.com>
2022-05-12 18:32:49 +0100

    shell script update

--- shell_script.sh
+++ shell_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-export PATH=/vol/bitbucket/ajl115/multilingual-psychotherapy-chatbot/satbot/bin/:$PATH
+export PATH=/vol/bitbucket/ajl115/myvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 TERM=vt100 # or TERM=xterm
