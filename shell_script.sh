7696bc2dfbc6da126582a4bfd5bf20254ddc6bf1
alicialawjy <alicialawjy@gmail.com>
2022-05-12 18:00:57 +0100

    shell script

---
+++ shell_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+export PATH=/vol/bitbucket/ajl115/multilingual-psychotherapy-chatbot/satbot/bin/:$PATH
+source activate
+source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
+TERM=vt100 # or TERM=xterm
+python3 emotion_classifier.py
+/usr/bin/nvidia-smi
+uptime
