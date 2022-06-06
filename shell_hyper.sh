e2b71a2be77aa684279793282b831a1eda4012cf
alicialawjy <alicialawjy@gmail.com>
2022-06-06 11:49:02 +0100

    shell script for hyperparam tuning

---
+++ shell_hyper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+export PATH=/vol/bitbucket/ajl115/myvenv/bin/:$PATH
+source activate
+source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
+TERM=vt100 # or TERM=xterm
+python3 emotion_classifier_hypertune.py
+/usr/bin/nvidia-smi
+uptime
