827466645be9de1fd054a2ed36e0efe6b7fc90d8
alicialawjy <alicialawjy@gmail.com>
2022-06-08 14:17:20 +0100

    double finetune

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader # for the dataloader
+import pickle
+from torch.utils.data import Dataset, DataLoader 
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
+from transformers import Trainer, TrainingArguments
 from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 # Convert dataframe into dictionary of text and labels
 def reader(df):
     texts = df['text'].values.tolist()
-    labels = df['label'].values.tolist()
+    labels = df['labels'].values.tolist()
 
     return {'texts':texts, 'labels':labels}
 
@@ -25,7 +27,7 @@ class OlidDataset(Dataset):
 
     for b in batch:
       texts.append(str(b['text']))
-      labels.append(b['labels'])
+      labels.append(b['label'])
 
     print(texts)
     print(labels)
@@ -34,8 +36,8 @@ class OlidDataset(Dataset):
       return_tensors = 'pt',        # return pytorch tensors
       add_special_tokens = True,    # incld tokens like [SEP], [CLS]
       padding = "max_length",       # pad to max sentence length
-      truncation = True)            # truncate if too long
-      # max_length= 128              
+      truncation = True,            # truncate if too long
+      max_length= 128)              
 
     encodings['labels'] = torch.tensor(labels)
     return encodings
@@ -48,57 +50,112 @@ class OlidDataset(Dataset):
 
     return item
 
-# train model
-def train_model():
+
+def train_model(model_name, epoch, output_dir, train_df, eval_df):
   optimizer = 'AdamW' 
   learning_rate = 4e-05
-  epochs = 7
   
-  model_args = ClassificationArgs(num_train_epochs=epochs, 
-                                        no_save=True, 
-                                        no_cache=True, 
-                                        save_steps=-1,
-                                        save_model_every_epoch=False,
-                                        overwrite_output_dir=True,
-                                        learning_rate=learning_rate,
-                                        optimizer=optimizer)
+  model_args = ClassificationArgs(num_train_epochs=epoch,        # number of epochs
+                                  # best_model_dir=best_model_dir,  # directory to save best model
+                                  # evaluate_during_training=False,  # best model determined by validation set performance
+                                  no_cache=True,                  
+                                  save_steps=-1,                  
+                                  save_model_every_epoch=False,
+                                  output_dir = output_dir,
+                                  overwrite_output_dir = True,
+                                  learning_rate=learning_rate,    # learning rate
+                                  optimizer=optimizer)            # optimizer
 
-  model = ClassificationModel(model_type="xlmroberta",      # tried xlmroberta, bert
-                            model_name="xlm-roberta-base",  # tried bert-base-chinese, xlm-roberta-base, bert-base-multilingual-cased (mBert), microsoft/infoxlm-base
-                            args = model_args, 
-                            num_labels=4,
-                            use_cuda=cuda_available)
-  
-  model.train_model(df_train[['text', 'labels']])
-  model.save_model('finetuned-model/')
+  model = ClassificationModel(model_type="xlmroberta",  # tried xlmroberta, bert
+                            model_name=model_name,      # tried bert-base-chinese, xlm-roberta-base, bert-base-multilingual-cased (mBert), microsoft/infoxlm-base
+                            args = model_args,          # see above
+                            num_labels=4,               # 4 labels - sad, happy, fear, anger
+                            use_cuda=cuda_available)    # use GPU
 
-  # # Validation Set (Internal)
-  # y_pred, _ = model.predict(df_val.text.tolist())
-  # y_true = df_val['labels']
+  model.train_model(train_df = train_df,                # training dataset
+                    eval_df = eval_df)                  # evaluation dataset
+                 
+  return model
 
-  # print("Classification Report")
-  # print(f1_score(y_true, y_pred,average='weighted'))
-  # print(classification_report(y_true, y_pred))
-  # print(confusion_matrix(y_true, y_pred))
-  
-  # Test Set (Internal)
-  y_pred, _ = model.predict(df_test.text.tolist())
-  y_true = df_test['labels']
-  print("Test Set Classification Report")
-  print(classification_report(y_true, y_pred))
-  print(confusion_matrix(y_true, y_pred))
 
+def evaluate(model, df_dataset):
+  y_pred, _ = model.predict(df_dataset.text.tolist())
+  y_true = df_dataset['labels']
+
+  print("Classification Report", classification_report(y_true, y_pred))
+  print("Confusion Matrix", confusion_matrix(y_true, y_pred))
+  print("F1-Score", f1_score(y_true, y_pred,average='weighted'))
+  return f1_score(y_true, y_pred,average='weighted')
+
+# Run double finetuning
 if __name__ == "__main__":
-    GPU = True
-    if GPU:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    else:
-        device = torch.device("cpu")
-    print(f"Using {device}")
+  ## Datasets
+  # Emotion (Twitter) Dataset (First Tune)
+  df_train_twitter = pd.read_csv('data/emotions/twitter/twitter_emotions_enzh.csv')
 
-    cuda_available = torch.cuda.is_available()
-    df_train = pd.read_csv('data/emotions/EmpatheticPersonas/EN-ZH/emotionlabeled_train.csv')
-    df_val = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
-    df_test = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
+  # EmpatheticPersonas (EP) Dataset (Second Tune)
+  df_train_EP = pd.read_csv('data/emotions/EmpatheticPersonas/EN-ZH/emotionlabeled_train.csv')
+  df_val_EP = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
+  df_test_EP = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
 
-    train_model()
+  # Use GPU
+  GPU = True
+  if GPU:
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  else:
+      device = torch.device("cpu")
+  print(f"Using {device}")
+
+  cuda_available = torch.cuda.is_available()
+
+  # Begin First Finetune 
+  model = train_model(model_name = "xlm-roberta-base",
+                      epoch = 3,
+                      output_dir = 'emotion_classifier/outputs/first-tune-twitter',
+                      train_df = df_train_twitter[['text','labels']],
+                      eval_df = df_val_EP[['text','labels']])
+
+  # Evaluate FINAL model F1
+  F1 = evaluate(model, df_val_EP)
+
+  # Second Finetune (Hyperparam Tune)
+  best_F1 = 0
+  best_epoch = 1
+
+  for epoch in range(1,10):
+    model = train_model(model_name = "emotion_classifier/outputs/first-tune-twitter",
+                        output_dir = 'emotion_classifier/outputs/second-tune-EP/'+str(epoch),
+                        epoch = epoch,
+                        train_df = df_train_EP[['text','labels']],
+                        eval_df = df_val_EP[['text','labels']])
+
+    # evaluate each epoch's performance
+    F1 = evaluate(model, df_val_EP)
+    if F1 > best_F1:
+      best_F1 = F1
+      best_epoch = epoch
+
+  # Load the best model
+  model_args = ClassificationArgs(num_train_epochs=best_epoch, 
+                                    #best_model_dir = '/content/drive/MyDrive/emotion_classifier/outputs/second-tune-EP',
+                                    #evaluate_during_training=True,
+                                    no_cache=True, 
+                                    save_steps=-1,
+                                    save_model_every_epoch=False,
+                                    overwrite_output_dir=True,
+                                    learning_rate=4e-05,
+                                    optimizer='AdamW')
+
+  model_best = ClassificationModel(model_type="xlmroberta",      
+                              model_name='emotion_classifier/outputs/second-tune-EP/'+str(best_epoch),  
+                              args = model_args, 
+                              num_labels=4,
+                              use_cuda=cuda_available)
+
+  # check if same results obtained
+  print('Evaluating best model')
+  evaluate(model_best, df_val_EP)
+
+  # Evaluate saved model on test results
+  print('Best Model on Held-Out Test Set')
+  evaluate(model_best, df_test_EP)
