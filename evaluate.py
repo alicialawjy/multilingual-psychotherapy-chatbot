f1e285746f173e835414e01f3e50554665e84e4e
alicialawjy <alicialawjy@gmail.com>
2022-06-08 23:59:35 +0100

    evaluation file

---
+++ evaluate.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import torch
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+from simpletransformers.classification import ClassificationModel
+
+def evaluate(model, df_dataset):
+  y_pred, _ = model.predict(df_dataset.text.tolist())
+  y_true = df_dataset['labels']
+
+  print("Classification Report", classification_report(y_true, y_pred))
+  print("Confusion Matrix", confusion_matrix(y_true, y_pred))
+  print("F1-Score", f1_score(y_true, y_pred,average='weighted'))
+  return f1_score(y_true, y_pred,average='weighted')
+
+# test data
+df_EP_ZH_val = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
+df_EP_ZH = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
+df_EP_EN = pd.read_csv('data/emotions/EmpatheticPersonas/EN/emotionlabeled_test.csv')
+df_ZH_native = pd.read_csv('data/emotions/NLPCC2014/NLPCC2014(ZH-Native).csv')
+df_codeswitch = pd.read_csv('data/emotions/codeswitch/data/codeswitch.csv')
+
+# fill in with the best params 
+models = {'single': 'emotion_classifier/outputs/single-tune/{str(best_epoch)}/{str(best_lr)}', # single tune best
+'sentiment40k': 'emotion_classifier/outputs/second-tune-EP40k/{str(epoch)}/{str(lr)}', # sentiment-40k best
+'twitter': 'emotion_classifier/outputs/second-tune-EP40k/{str(epoch)}/{str(lr)}'} # twitter best
+
+for (ft, model_name) in models.items():
+    cuda_available = torch.cuda.is_available()
+
+    # Load the best model
+    model_best = ClassificationModel(model_type="xlmroberta", 
+                                    model_name=model_name, 
+                                    num_labels=4, 
+                                    use_cuda=cuda_available)
+    
+    print(f'Results for {ft} finetuning')
+
+    # 1: Sanity Check
+    print('Sanity Check on Validation Set')
+    evaluate(model_best, df_EP_ZH_val)
+
+    # 2: Test (ZH) Performance
+    print('ZH Test Set')
+    evaluate(model_best, df_EP_ZH)
+
+    # 3: Test (EN) Performance
+    print('EN Test Set')
+    evaluate(model_best, df_EP_EN)
+
+    # 4: Native ZH Performance
+    print('ZH Native Set')
+    evaluate(model_best, df_ZH_native)
+
+    # 5: Code Switch Performance
+    print('CodeSwitch Set')
+    evaluate(model_best, df_codeswitch)
+
