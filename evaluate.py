31fa37ff62bf160f02f4137c27664b7b300ec56d
alicialawjy <alicialawjy@gmail.com>
2022-06-09 16:37:22 +0100

    evaluate with roy-native dataset

--- evaluate.py
+++ evaluate.py
@@ -14,10 +14,10 @@ def evaluate(model, df_dataset):
 
 # test data
 # df_EP_ZH_val = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
-df_EP_ZH = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
-df_EP_EN = pd.read_csv('data/emotions/EmpatheticPersonas/EN/emotionlabeled_test.csv')
-df_ZH_native = pd.read_csv('data/emotions/NLPCC2014/NLPCC2014(ZH-Native).csv')
-df_codeswitch = pd.read_csv('data/emotions/EmpatheticPersonas/EP_codeswitch.csv')
+# df_EP_ZH = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
+# df_EP_EN = pd.read_csv('data/emotions/EmpatheticPersonas/EN/emotionlabeled_test.csv')
+df_ZH_native = pd.read_csv('/Users/alicialawjy/Desktop/multilingual-psychotherapy-chatbot/data/emotions/Native Dataset/roy_native.csv') # ('data/emotions/NLPCC2014/NLPCC2014(ZH-Native).csv')
+# df_codeswitch = pd.read_csv('data/emotions/EmpatheticPersonas/EP_codeswitch.csv')
 
 # fill in with the best params 
 models = {'single': 'emotion_classifier/outputs/single-tune/5/3e-05', # single tune best
@@ -39,19 +39,21 @@ for (ft, model_name) in models.items():
     # print('Sanity Check on Validation Set')
     # evaluate(model_best, df_EP_ZH_val)
 
-    # 2: Test (ZH) Performance
-    print('ZH Test Set')
-    evaluate(model_best, df_EP_ZH)
+    # # 2: Test (ZH) Performance
+    # print('ZH Test Set')
+    # evaluate(model_best, df_EP_ZH)
 
-    # 3: Test (EN) Performance
-    print('EN Test Set')
-    evaluate(model_best, df_EP_EN)
+    # # 3: Test (EN) Performance
+    # print('EN Test Set')
+    # evaluate(model_best, df_EP_EN)
 
     # 4: Native ZH Performance
     print('ZH Native Set')
     evaluate(model_best, df_ZH_native)
 
-    # 5: Code Switch Performance
-    print('CodeSwitch Set')
-    evaluate(model_best, df_codeswitch)
+    # # 5: Code Switch Performance
+    # print('CodeSwitch Set')
+    # evaluate(model_best, df_codeswitch)
 
+# ran on job 52784
+# 52789 for cleaned en test dataset
