b67ebebeb634f0b2d362211440fe741a27d39b9b
alicialawjy <alicialawjy@gmail.com>
2022-06-17 14:55:34 +0100

    evaluate ECM at 1e-05 and 3e-05

--- evaluate.py
+++ evaluate.py
@@ -13,48 +13,67 @@ def evaluate(model, df_dataset):
   return f1_score(y_true, y_pred,average='weighted')
 
 # test data
-# df_EP_ZH_val = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
-df_EP_ZH = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
-df_EP_EN = pd.read_csv('data/emotions/EmpatheticPersonas/EN/emotionlabeled_test.csv')
-df_ZH_native = pd.read_csv('data/emotions/Native Dataset/roy_native.csv') # ('data/emotions/NLPCC2014/NLPCC2014(ZH-Native).csv')
-df_codeswitch = pd.read_csv('data/emotions/EmpatheticPersonas/EP_codeswitch.csv')
+df_ECM_test = pd.read_csv('data/emotions/sentiment-40k/sentiment-40k_test.csv')
+
+# models we want to test
+models = {'3e-05': 'emotion_classifier/2nd-run/2-tuned-ECM/1st-tuning/best-ECM',
+          '1e-05': 'emotion_classifier/2-tuned-ECM-1e05/1st-tuning/best-ECM'}
+
+for lr,model_name in models.items():
+  cuda_available = torch.cuda.is_available()
+
+  # Load the best model
+  model_best = ClassificationModel(model_type="xlmroberta", 
+                                  model_name=model_name, 
+                                  num_labels=4, 
+                                  use_cuda=cuda_available)
+
+  print(f'ECM finetuning results for learning rate = {lr}')
+  evaluate(model_best, df_ECM_test)
+
+
+# # df_EP_ZH_val = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_val.csv')
+# df_EP_ZH = pd.read_csv('data/emotions/EmpatheticPersonas/ZH/emotionlabeled_test.csv')
+# df_EP_EN = pd.read_csv('data/emotions/EmpatheticPersonas/EN/emotionlabeled_test.csv')
+# df_ZH_native = pd.read_csv('data/emotions/Native Dataset/roy_native.csv') # ('data/emotions/NLPCC2014/NLPCC2014(ZH-Native).csv')
+# df_codeswitch = pd.read_csv('data/emotions/EmpatheticPersonas/EP_codeswitch.csv')
 
 # fill in with the best params 
-models = {'sentiment40k': 'emotion_classifier/outputs/second-tune-EP40k/5/3e-05'} # sentiment-40k best
+# models = {'sentiment40k': 'emotion_classifier/outputs/second-tune-EP40k/5/3e-05'} # sentiment-40k best
 # 'single': 'emotion_classifier/outputs/single-tune/5/3e-05',
 # 'sentiment40k': 'emotion_classifier/outputs/second-tune-EP40k/2/3e-05'
 # 'twitter': 'emotion_classifier/outputs/second-tune-EP/3/4e-05' 
 
-for (ft, model_name) in models.items():
-    cuda_available = torch.cuda.is_available()
+# for (ft, model_name) in models.items():
+#     cuda_available = torch.cuda.is_available()
 
-    # Load the best model
-    model_best = ClassificationModel(model_type="xlmroberta", 
-                                    model_name=model_name, 
-                                    num_labels=4, 
-                                    use_cuda=cuda_available)
+#     # Load the best model
+#     model_best = ClassificationModel(model_type="xlmroberta", 
+#                                     model_name=model_name, 
+#                                     num_labels=4, 
+#                                     use_cuda=cuda_available)
     
-    print(f'Results for {ft} finetuning')
+#     print(f'Results for {ft} finetuning')
 
-    # # 1: Sanity Check
-    # print('Sanity Check on Validation Set')
-    # evaluate(model_best, df_EP_ZH_val)
+#     # # 1: Sanity Check
+#     # print('Sanity Check on Validation Set')
+#     # evaluate(model_best, df_EP_ZH_val)
 
-    # 2: Test (ZH) Performance
-    print('ZH Test Set')
-    evaluate(model_best, df_EP_ZH)
+#     # 2: Test (ZH) Performance
+#     print('ZH Test Set')
+#     evaluate(model_best, df_EP_ZH)
 
-    # 3: Test (EN) Performance
-    print('EN Test Set')
-    evaluate(model_best, df_EP_EN)
+#     # 3: Test (EN) Performance
+#     print('EN Test Set')
+#     evaluate(model_best, df_EP_EN)
 
-    # 4: Native ZH Performance
-    print('ZH Native Set')
-    evaluate(model_best, df_ZH_native)
+#     # 4: Native ZH Performance
+#     print('ZH Native Set')
+#     evaluate(model_best, df_ZH_native)
 
-    # 5: Code Switch Performance
-    print('CodeSwitch Set')
-    evaluate(model_best, df_codeswitch)
+#     # 5: Code Switch Performance
+#     print('CodeSwitch Set')
+#     evaluate(model_best, df_codeswitch)
 
 # ran on job 52784
 # 52789 for cleaned en test dataset
