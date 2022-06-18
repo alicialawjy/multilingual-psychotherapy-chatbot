b30d2197692709d2c93e8e050483e9f5fb742cd8
alicialawjy <alicialawjy@gmail.com>
2022-06-18 02:44:06 +0100

    2e05

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -153,10 +153,10 @@ if __name__ == "__main__":
 
     # Second Finetune (EP - Hyperparam Tune)
     model = train_model(epoch = 20, 
-                        learning_rate = 9e-06, 
+                        learning_rate = 2e-05, 
                         model_name = 'emotion_classifier/2-tuned-ECM-9e06/1st-tuning/best-ECM', 
-                        best_model_dir = 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-9e06/best-final', 
-                        output_dir = 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-9e06/outputs', 
+                        best_model_dir = 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-2e05/best-final', 
+                        output_dir = 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-2e05/outputs', 
                         use_early_stopping = True, 
                         early_stopping_delta = 0.0001, 
                         early_stopping_metric = "eval_loss", 
@@ -170,7 +170,7 @@ if __name__ == "__main__":
 
     # load the best model for this epoch
     best_model = ClassificationModel(model_type="xlmroberta", 
-                                    model_name= 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-9e06/best-final', 
+                                    model_name= 'emotion_classifier/2-tuned-ECM-9e06/2nd-tuning-2e05/best-final', 
                                     num_labels=4, 
                                     use_cuda=cuda_available)
 
@@ -218,3 +218,4 @@ if __name__ == "__main__":
 # 54153: 28 with 7e-05
 # 54159: 28 with 5e-06
 # 54160: 28 with 1e-06
+# 54165: 28 with 9e-06
