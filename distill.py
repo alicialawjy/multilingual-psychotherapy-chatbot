595590ad85167b1b45014a33212019078892cded
alicialawjy <alicialawjy@gmail.com>
2022-06-20 23:53:46 +0100

    rerun 1e-05

--- distill.py
+++ distill.py
@@ -235,12 +235,12 @@ if __name__ == "__main__":
   # Student Models
   # First Finetuning (ECM)
   student_model = run_training(epoch = 20, 
-                              learning_rate = 3e-05,
+                              learning_rate = 1e-05,
                               alpha = 0.5,
                               temperature = 4,
-                              output_dir = 'distill/2-tune-2-teacher/1st-tune/3e-05/outputs', 
-                              best_model_dir = 'distill/2-tune-2-teacher/1st-tune/3e-05/best-model', 
-                              student_model_name = 'nreimers/mMiniLMv2-L6-H384-distilled-from-XLMR-Large',
+                              output_dir = 'distill/2-tune-2-teacher/1st-tune/1e-05/outputs', 
+                              best_model_dir = 'distill/2-tune-2-teacher/1st-tune/1e-05/best-model', 
+                              student_model_name = 'distill/2-tune-2-teacher/1st-tune/1e-05/outputs/checkpoint-3250', # 'nreimers/mMiniLMv2-L6-H384-distilled-from-XLMR-Large',
                               teacher_model = first_teacher_model, 
                               use_early_stopping = True,
                               early_stopping_delta = 0.0001,
@@ -256,11 +256,11 @@ if __name__ == "__main__":
   
   # load the best model from the first finetuning
   model_best_1st = ClassificationModel(model_type="xlmroberta", 
-                                      model_name= 'distill/2-tune-2-teacher/1st-tune/3e-05/best-model', 
+                                      model_name= 'distill/2-tune-2-teacher/1st-tune/1e-05/best-model', 
                                       num_labels=4, 
                                       use_cuda=cuda_available)
   
-  print('First Tuning (lr = 3e-05) Validation Results')
+  print('First Tuning (lr = 1e-05) Validation Results')
   evaluate(model_best_1st, df_test_ECM)
 
   # # # Second Finetuning (EP) 
