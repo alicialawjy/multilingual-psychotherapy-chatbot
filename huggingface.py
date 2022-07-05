e0177d6aaa19bcaf914820fd9f6e446aa85b4244
alicialawjy <alicialawjy@gmail.com>
2022-07-05 21:40:41 +0100

    reduce block size and batch size

--- huggingface.py
+++ huggingface.py
@@ -5,7 +5,7 @@ def load_dataset(train_path, tokenizer):
     train_dataset = TextDataset(
           tokenizer=tokenizer,
           file_path=train_path,
-          block_size=128)
+          block_size=100)
      
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False,
@@ -22,7 +22,7 @@ training_args = TrainingArguments(
     output_dir="rewriting/gpt2-ep", #The output directory
     overwrite_output_dir=True,      #overwrite the content of the output directory
     num_train_epochs=3,             # number of training epochs
-    per_device_train_batch_size=16, # batch size for training
+    per_device_train_batch_size=4, # batch size for training
     # per_device_eval_batch_size=64,  # batch size for evaluation
     # eval_steps = 400,             # Number of update steps between two evaluations.
     save_steps=800,                 # after # steps model is saved 
@@ -46,4 +46,4 @@ trainer.save_model()
 ep_generator = pipeline('text-generation',model='rewriting/gpt2-ep', tokenizer='sberbank-ai/mGPT',config={'max_length':50})
 ep_generator('悲伤 - 这是由最近或遥远的事件（或多个事件）引起的吗?')[0]['generated_text']
 
-# 55759: 
+# 55764:  
