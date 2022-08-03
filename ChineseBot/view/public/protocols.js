7c1f556a789fdc35f4f1ff55d5ae98c955954446
alicialawjy <alicialawjy@gmail.com>
2022-08-03 14:52:36 +0100

    remove iframe and use html

---
+++ ChineseBot/view/public/protocols.js
@@ -0,0 +1,254 @@
+var protocol_dict = {
+    "1_EN": {
+        "title": "Connecting with the Child",
+        "content": "Try to imagine the happy childhood photo/avatar and reflect on relevant positive affects, then imagine the unhappy photo and relevant negative affects. Repeat many times until this is easy to do. Try to imagine that the child, as you were, is near you (either in happy or unhappy state), and then imagine that you are embracing/cuddling the child. You can also imagine playing with the child. "
+    },
+    "2_EN": {
+        "title": "Laughing At Our Two Childhood Pictures",
+        "content": "Begin by laughing at the childhood pictures, then think about why we laugh at these pictures, i.e. laugh at the contrast between them (Incongruity theory), laugh since your self is now superior than in the past (Superiority theory) and laugh because “Life is a tragedy when seen in close-up, but a comedy in long-shot” (Charlie Chaplin). Remember that we do not laugh at them to ridicule. This process will allow us to teach our childhood self to laugh. "
+    },
+    "3_EN": {
+        "title": "Falling In Love With The Child",
+        "content": "While looking at the happy childhood photo, recite selected happy love songs and imagine that you are establishing a deep emotional bond with the childhood self. Then sing with a loud voice, gradually using your whole body as if dancing with the child and having a loving dialogue. \
+        "
+    },
+    "4_EN": {
+        "title": "Vow To Adopt The Child As Own Child",
+        "content": "You imaginatively adopt your childhood self as your own child, loudly pledging to consistently support your child in any way possible. The pledge must be life-long and must be reinforced over time through practising Self-Attachment protocols. \
+        "
+    },
+    "5_EN": {
+        "title": "Maintaining A Loving Relationship With The Child",
+        "content": "Choose a short phrase e.g. “You are my beloved” and repeatedly utter it while focusing on the happy and unhappy childhood photos. Recite one or two happy love songs, loudly repeating these using your whole body. \
+        "
+    },
+    "6_EN": {
+        "title": "An Exercise To Process The Painful Childhood Events",
+        "content": "With closed eyes, recall a painful scene from childhood e.g. emotional or physical abuse in as much detail as possible, and associate the face of the child you were with your unhappy photo. After recalling this event and the related emotions, imagine your adult self approaching and embracing the child like a parent embracing a child in distress. <br><br> \
+        While your eyes are still closed, continue to imagine supporting and cuddling the child, loudly supporting them (Examples: “Why are you hitting my child?” and “My darling, I will not let them hurt you more.”). Massage your face while doing so, which we interpret as cuddling the child. \
+        "
+    },
+    "7_EN": {
+        "title": "Protocols For Creating Zest For Life ",
+        "content": "Using a mirror, imagine the reflection is your childhood self and loudly recite to it your selected happy love songs, using your entire body. Repeat songs and poems in many different circumstances e.g. while walking on the street or doing housework, to be able to integrate them into your life. \
+        "
+    },
+    "8_EN": {
+        "title": "Loosening Facial And Body Muscles",
+        "content": "You should loosen your muscles at least twice a day as you sing with your animated face and entire body, as if playing, dancing, laughing and having fun with the child as parents interact with children. \
+        "
+    },
+    "9_EN": {
+        "title": "Protocols For Attachment And Love Of Nature",
+        "content": "To create an attachment with nature, you should visit a park or forest and spend time admiring nature, e.g. admiring a beautiful tree, as if seeing its branches and leaves at a deeper level for the first time. Repeat continuously and with different trees until you feel you have formed an attachment with nature. This will help to modulate your emotions and you will want to spend more time with nature each day. \
+        "
+    },
+    "10_EN": {
+        "title": "Laughing At, And With One’s Self",
+        "content": "Begin laughing with yourself about a small accomplishment e.g. in sports, housework, or any other task, however small or unimportant. With every small accomplishment, you should smile as if victorious, and gradually change this smile to laughter, and make this laughter last longer and longer. By practising this you will be able to smile and laugh without ridicule about anything you have said or done in the past while maintaining compassion for your childhood self. \
+        "
+    },
+    "11_EN": {
+        "title": "Processing Current Negative Emotions",
+        "content": "With closed eyes, imagine the unhappy photo and project the unhappy emotions, e.g. anger, sorrow, towards the photo that represents the child. As with Type 6, we make contact with our adult self to attend to and care for the child to support the child and modulate the child’s negative emotions. <br><br> \
+        While projecting these negative emotions, loudly reassure the child and massage your own face, which we interpret as cuddling the child. Continue this until you have contained the negative emotions, at which point you can switch to focusing on the happy photo. \
+        "
+    },
+    "12_EN": {
+        "title": "Continuous Laughter",
+        "content": "At a time when you are alone, open your mouth slightly, loosen your face muscles, form a Duchenne smile and slowly repeat one of the following phrases as if laughing: eh, eh, eh, eh; ah, ah, ah, ah; oh, oh, oh, oh; uh, uh, uh, uh; or ye, ye, ye, ye. <br><br> \
+        If a subject is needed for laughter, you can think about the silliness of the exercise. This exercise is a good antidote for stress. \
+        "
+    },
+    "13_EN": {
+        "title": "Changing Our Perspective For Getting Over Negative Emotions",
+        "content": "To break free of powerful negative patterns that emerge when we are stuck in the swamp of negative emotions, or a “psychological abyss”, stare at the black vase in the Gestalt vase picture (below). When your perception changes and you see the white faces, convince yourself that these abysses can be overcome and try to laugh out loud as a victory sign. <br><br> \
+        Having created a positive powerful pattern of love with the child through previous exercises, you can now depart from the field of negative patterns by singing your happy love song to enter the attractive field of love for the child instead.  <br><br> \
+        This is like changing our interpretation of the below image. Instead of seeing a black vase of negative emotions discovering two white faces, you see the child and the adult self who are now looking at each other.  <br><br> \
+        <img id='gestalt-vase' src='https://cdn.pixabay.com/photo/2017/10/16/22/07/cranium-2858764_1280.png'> \
+        "
+    },
+    "14_EN": {
+        "title": "Protocols For Socializing The Child",
+        "content": "By repeating protocols 1-13 you can reduce negative emotions and increase positive affects. You are gradually able to perform these exercises with eyes open and can integrate them into your daily life. You should be able to extend compassion for the child to other people. The adult self should become aware of any narcissistic tendencies or anti-social feelings of the child e.g. envy, jealousy, greed, hatred, mistrust, malevolence, controlling behavior and revengefulness. <br><br> \
+        The adult self can behave like a parent to contain these emotions and discourage acting out any anti-social feelings and attitudes of the child by expressing affection to the child and simulating cuddles by massaging your face. <br><br> \
+        The adult self should try to direct the child’s anger and negative energy towards playing, creativity and development. <br><br> \
+        As the child’s positive affects increase and his/her negative affects decrease, by expressing positive emotions he/she can attract more positive reactions from others, and in turn gain a more positive outlook toward others. \
+        "
+    },
+    "15_EN": {
+        "title": "Recognizing And Controlling Narcissism And The Internal Persecutor",
+        "content": "The adult self becomes aware of the facets of the trauma triangle: internal persecutor, victim, and rescuer. The adult self examines the effects of the triangle (narcissism, lack of creativity) in daily life and previous experiences. <br><br>\
+        Your adult self can then review an important life experience and your social and political views as an adult, with awareness of how the internal persecutor operates. Your adult self can then create a list of examples from your experiences on how the internal persecutor operates, and carefully analyse these for examples of being drawn to trauma, being traumatized by the internal persecutor, and projecting the internal persecutor. You should be able to then re- evaluate your own experiences, cotain the internal persecutor and narcissistic tendencies and be able to develop creativity. \
+        "
+    },
+    "16_EN": {
+        "title": "Creating An Optimal Inner Model ",
+        "content": "With awareness of the internal persecutor, we will recognise emotions of the child that were learned from parents or through interactions with them. With the guidance of the adult self, who can transfer compassion for the child to others, the child will learn to avoid projecting the internal persecutor (which would lead to them becoming the victim or rescuer). \
+        "
+    },
+    "17_EN": {
+        "title": "Solving Personal Crisis",
+        "content": "In the midst of a personal crisis, as you continue to practice the protocol for modulating negative affects and the protocol for laughter, ask your child the following: <br> \
+        <ul> \
+            <li>How can you see the crisis as a way of becoming stronger? (ha ha ha) \
+            <li>How can you interpret the crisis as a way of reaching your high goal? (ha ha ha) \
+            <li>Has the internal persecutor been projecting onto others again? \
+            <li>\The adult self asks the following questions: \
+            <li>What is the similarity between this crisis and those faced before? How is it similar to the family crisis experienced as a child? Aren’t the other person’s positive attributes greater than his/her negative ones? How would a mature person interpret the crisis in comparison to my child? Can I see it from the perspective of someone else? Can I put myself in their place and understand their affects? Given my new inner working model can I find a way to calm the people involved in the crisis so we can find a better solution for it? \
+            <li>If not, can I respectfully maintain my distance and end the fight? \
+        "
+    },
+    "18_EN": {
+        "title": "Laughing At The Harmless Contradiction Of Deep-Rooted Beliefs",
+        "content": "First, visualize a painful event that took place in the distant past that you have struggled with for a long time, and despite its painfulness try to see it has led to some positive impact, lesson, change, or decision. We start with a painful event that happened in the distant past, so that by now we have been able to adjust our negative affects toward it. After repeated daily exercises, once we have experienced the forceful effectiveness of laughing at distant problems, we can gradually begin to laugh at more recent painful memories. <br><br>\
+        In expectation of hearing a funny joke we loosen our facial muscles, slightly open our mouths, and to grasp the incongruity in the joke we move our eyebrows up as a sign of surprise. As we repeat the sentences out loud, we slowly begin to laugh as we wait for the second part. And once we get to the first sentence of the second part, which is in complete contrast to our beliefs, we laugh out loud.<br><br>\
+        Not only should you: bear it, accept it, try to deal with it, tolerate its memory, try harder to endure its memory, adapt yourself to its memory, analyze and understand it and by doing so modulate your negative emotions and learn lessons for the future, try to soften your thoughts, depressive emotions, and anxieties, try to ... <br><br>\
+        Like Nietzsche’s wish consider it a great treasure (ha ha ha...), cherish it with great love (ha ha ha...), welcome its challenges with all your heart (ha ha ha...), consider it a good omen with all your heart (ha ha ha...), consider its challenges a great fortune (ha ha ha...), celebrate its memory (ha ha ha...), celebrate its memory with great joy (ha ha ha...), consider it a true love (ha ha ha...), consider it a true love with great passion and intimacy (ha ha ha...) ... <br><br> \
+        After repeated practice of the laughing exercises you can begin to apply it to things that worry you in the present and the future. \
+        "
+    },
+    "19_EN": {
+        "title": "Changing Ideological Frameworks For Creativity",
+        "content": "We challenge our usual ideological framework to weaken one-sided patterns and encourage spontaneity and examination of topics from multiple perspectives. Practice with subjects that you have deep-rooted beliefs and are excited about (e.g. anything from political/social issues to ideas on marriage and sexuality). \
+        For instance, examine the topic of racism and consider whether you have any latent racism and consider this subject in the dual role of proponent and opponent. <br><br>\
+        Repeat with topics where you may have stronger views e.g. marriage and sexual orientation. If you are politically in the center, consider the subject both from a leftist and rightist point of view and try to understand both sides of the issue and see the subject from three perspectives. \
+        "
+    },
+    "20_EN": {
+        "title": "Affirmations",
+        "content": "Put together a list of affirmations by different important figures. Choose ones that have an impact on you from the start and can provide you with strength in the long path for reaching your ultimate goal. Read them out loud. A few examples: <br> \
+            <ul> \
+                <li>My formula for greatness in a human being is Amor Fati: that one wants nothing to be other than it is, not in the future, not in the past, not in all eternity.” (Nietzsche) </l> \
+                <li>“I assess the power of a will by how much resistance, pain, torture it endures and knows how to turn it to its advantage.” (Nietzsche) </l> \
+                <li>Life is not easy. At times we inevitably suffer from hopelessness and paranoia unless if we have an ideal goal that helps us surpass suffering, weakness, and betrayals.” (Bronstein) </l> \
+            </ul> \
+        "
+    },
+    "1_ZH": {
+        "title": "与童年版的自己建立关系",
+        "content":"此练习的目的是鼓励用户与自己童年的自我建立关系。用户试图想象童年的自我，并想象与童年的自我 拥抱或玩耍。用户可以使用两张自己儿童时期的照片协助想象过程；一张开心的照片，一张闹脾气时的 照片。\
+        "
+    },
+    "2_ZH": {
+        "title": "对两张自己童年的照片笑",
+        "content":"该练习要求用户试图鼓励童年的自己笑。通过用户对自己的两张照片（练习1）笑，并同时考虑两张照 片中童年的自己所表示的情绪，从其作出对比。此练习的目的是鼓励用户实现与快乐的童年记忆产生共 鸣，同时笑对童年的烦恼。用户应该明白此练习中的笑并不是嘲笑自己，而是为了教我们童年的自己怎么笑。\
+        "
+    },
+    "3_ZH": {
+        "title": "对童年的自我示爱",
+        "content":"次练习以练习1作为基础，鼓励用户与童年的自己建立跟深的关系。用户对童年的自己播放并唱出充满 爱的歌曲（比如摇篮曲或儿歌），用户可以试图在唱的过程中起舞，想象自己是与童年的自我一起跳 舞。 \
+        "
+    },
+    "4_ZH": {
+        "title": " 许诺关怀童年的自己",
+        "content":"用户可以想象对童年的自己作出永远爱护的承诺，并以坚持实行SAT方案为支持童年自我的方式。鼓励 用户实际说出以上的话，并重复多次。\
+        "
+    },
+    "5_ZH": {
+        "title": "与童年的自己保持爱护的关系",
+        "content":"该练习目的为培养并保持用户与童年的自己建立的关系。用户可以选择一段向童年的自己示爱的短句， 并向自己的两张童年照片念。在实行此练习的过程中鼓励用户同时播放歌曲，随其起舞（练习3）。\
+        "
+    },
+    "6_ZH": {
+        "title": "处理痛苦的童年回忆",
+        "content":"该练习目的为帮助用户处理任何痛苦的童年回忆。此练习需要用户的想象童年的自己经历此事件，并经 可能得关注细节，用户可以使用自己童年的照片而协助以上过程。然后用户试图想象成年的自己作为家 长的角色去安慰并拥抱童年的自己。鼓励用户说出任何童年人物与成年人物之间的对话，并且在以上过 程中对自己面部按摩。\
+        "
+    },
+    "7_ZH": {
+        "title": "对生活产生热情",
+        "content":"此练习要求用户在镜子前作出各种活动（唱歌，跳舞，朗诵）。鼓励用户在以上过程中想象镜子中的反 射为童年的自我。次练习最好在一日中重复多次，并可以在不同情况下实行。\
+        "
+    },
+    "8_ZH": {
+        "title": "放松脸部及身体肌肉",
+        "content":"此练习鼓励用户放松面部及身体肌肉，并同时想象与童年的自己玩耍或拥抱。\
+        "
+    },
+    "9_ZH": {
+        "title": "对大自然与环境产生欣赏",
+        "content":" 鼓励用户与大自然保持互助的关系，常访周边绿色环境，并欣赏大自然的美妙。此练习需要用户到一个 户外环境，将注意力集中到一个对象（比如，一棵大树），观察此对象的细节（树枝，树叶）。鼓励用 户长期在多个地点实施此练习。\
+        "
+    },
+    "10_ZH": {
+        "title": "欣赏自己的成就，无论大小",
+        "content":"用户在此练习中选出自己的一个成就，无论大小，并对作出此成就微笑，逐渐从微笑发展到大笑。此练 习的目的是通过对自己成就笑培养用户笑对任何情况的能力，以提高用户面对负面情绪的能力。在此重 复此练习中的笑与练习2一样是来自内心的，自然的笑，并不是嘲笑。\
+        "
+    },
+    "11_ZH": {
+        "title": "通过自我安慰而面对负面情绪",
+        "content":"在此练习中，用户将任何负面情绪投射到童年的自我身上，想象童年的自我处在负面情绪状态下。用户 接下来试图安慰并鼓励童年的自我，将任何安慰或鼓励的话语说出来。在负面情绪解除后，用户可以想 象童年的自我感到满足的模样，可以使用自己童年的照片以助想象。\
+        "
+    },
+    "12_ZH": {
+        "title": "以笑减压",
+        "content":"此练习目的为帮助用户处理并减少心里压力。用户需在安静环境下实行次练习，用户试图使用所有面部 肌肉形成杜兴微笑 1 。鼓励用户在笑的同时发出这些声音： ‘eh, eh, eh, eh’; ‘ah, ah, ah, ah’; ‘oh, oh, oh, oh’; ‘ye, ye, ye, ye’ 又鼓励用户在实行此练习时尝试想象一些幽默/滑稽的事件以助于大笑。用户也 可以对此练习的滑稽而笑。\
+        "
+    },
+    "13_ZH": {
+        "title": "通过更换个人观点而处理幅面情绪",
+        "content":"此练习需要用户观看一个鲁宾花瓶图案  ，试图将不同看图的视角投射到引起当前负面情绪的事情上， 从而用过不同的视角观看问题。鼓励用户将黑瓶子联想成当前的负面情绪，而图中的两个白脸联想成先 前以建立关系的成年自我与童年自我，看图时的视觉的转换联想为对事件的负面视角转为乐观视角。<br><br>\
+        <img id='gestalt-vase' src='https://cdn.pixabay.com/photo/2017/10/16/22/07/cranium-2858764_1280.png'> \
+        "
+    },
+    "14_ZH": {
+        "title": "认识童年自我的不良行为",
+        "content":"在此练习中，鼓励用户从成年自我的角色将自己的行为,表现投射到童年自我的身上，从而认识任何童年 自己的不良行为，并试图成熟得教导童年的自我如何克服不良行为。此练习的目的为通过这种方式试图 帮助用户识别自我的不良行为并克服。\
+        "
+    },
+    "15_ZH": {
+        "title": "认识并控制过度自恋或自责行为",
+        "content":"该练习需要用户熟悉心理创伤三点：迫害情结，受害者情结，救星情结，并以此作为基础而分析自己的 与其相关的经历，特别是认识这些经历所导致的过度自恋或自责行为。通过分析自己的经历去识别并预 防过度自恋或自责行为。\
+        "
+    },
+    "16_ZH": {
+        "title": "优化自我内心状态",
+        "content":"用户在此练习中回忆自己童年时期所受的教养并分析自己与父母的关系，以及以上对用户今日的性格行 为的影响。然后鼓励用户想象将现在的负面行为/性格投射到童年自我上，并努力教育童年自我如何改 善自己的行为。\
+        "
+    },
+    "17_ZH": {
+        "title": "解决个人危机",
+        "content":"此协议针对正在生活中经历个人危机的用户，例如与朋友/家人闹翻等。用户首先将现在的情绪投射到 童年的自我，然后在练习15的基础上询问童年的自我此事件是否跟练习15的三点有关。完成后用户反省 这次事件是否是个自我提升的机会。鼓励用户在实行以上步骤时尝试练习12。最终用户从成年自我的角 度分析自己的经历，与曾经的经历做对比，并分析自己是否有可以提升的地方。\
+        "
+    },
+    "18_ZH": {
+        "title": "认出无害矛盾性并从其取乐",
+        "content":"在此练习中用户阅读一段尼采的“权利意志”的话，并边读边笑。鼓励用户在读的过程中回忆曾经的负面 经历，和这些经历对用户今日的自我的影响。此练习的目的是训练用户去笑对过去的负面事件，从而可 以逐渐适应面对新的负面事件以及所产生的负面情绪。\
+        "
+    },
+    "19_ZH": {
+        "title": "通过体验不同思想框架而提高想象力",
+        "content":"练习19鼓励用户强迫自己考虑不同的思想模式，或在特别问题上挑战自己已有的想法，例如政治问题， 社会问题（种族，性别问题，等）。鼓励用户在练习过程中形成两个角色，交换时辩论问题，并试图了 解多个视角。\
+        "
+    },
+    "20_ZH": {
+        "title": "利用名言实施自我鼓励",
+        "content":"此练习要求用户采集一系列名言，从其选出部分用户感觉深有同感的，通过朗读从中寻找鼓励，自信。\
+        "
+    },
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Default view
+    
+    // Direct to appropriate protocol view based on protocol selected
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            // Log the click
+            console.log(e.target.id + ' clicked')  
+
+            // Retrieve link id
+            let protocol_id = e.target.id;
+            
+
+            // Display content in viewer
+            let title = protocol_dict[protocol_id]["title"]
+            let content = protocol_dict[protocol_id]["content"]
+
+            document.querySelector('.protocol_title').innerHTML = title
+            document.querySelector('.protocol_content').innerHTML = content
+        });
+    });
+    
+});
+
